11637ab99aa39db5de5d692bdfbf7e4c9a6951ca
jeevashhkjs <jeevasdckap@gmail.com>
2023-05-19 13:41:55 +0530

    implemented prompt function

--- is_PrimeNumber.php
+++ is_PrimeNumber.php
@@ -1,17 +1,24 @@
 <?php
 
 function isPrimeNumber($userInput){
-    $checkNumber;
-    for($i = 2;$i < $userInput;$i++){
-        if($userInput % $i == 0){
-            $checkNumber = "It's not Prime Number";
-            break;
-        }
-        else{
-            $checkNumber = "It's a Prime Number";
+    $checkNumber = "";
+    if($userInput === 1){
+        echo "1 is a Prime Number";
+    }else{
+        for($i = 2;$i < $userInput;$i++){
+            if($userInput % $i == 0){
+                $checkNumber = $userInput." It's not Prime Number";
+                break;
+            }
+            else{
+                $checkNumber = $userInput." It's a Prime Number";
+            }
         }
     }
+
     return $checkNumber;
 };
 
-echo isPrimeNumber(83);
+$a = readline('Enter a Number: ');
+
+echo isPrimeNumber($a);
