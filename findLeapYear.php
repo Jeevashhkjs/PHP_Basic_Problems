b21ffcbf247f0873cd5938faf409667a0891414c
JeevaJas <113868501+Jeevashhkjs@users.noreply.github.com>
2023-06-01 11:13:56 +0530

    Update findLeapYear.php

--- findLeapYear.php
+++ findLeapYear.php
@@ -3,7 +3,7 @@
 $userInput = readLine("Please enter a year ");
 
 function findLeapYear($getUserInput){
-    if($getUserInput % 400 == 0 || $getUserInput % 4 == 0 || $getUserInput % 100 != 0){
+    if($getUserInput % 400 == 0 || $getUserInput % 4 == 0 || $getUserInput % 100 == 0){
         echo "It's a Leap Year";
     }
     else{
